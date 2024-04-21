d625a0962c49934fe68c454524d445444756c005
luisgs7 <luisgsilva270@gmail.com>
2024-04-21 11:03:44 -0300

    new components Mine and Field

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react"
 import { StyleSheet, Text, View } from "react-native"
 import params from "./src/params"
+import Field from "./src/components/Field"
 
 
 export default class App extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Iniciando o Mines!!!</Text>
           <Text style={styles.instructions}>
           Tamanho da grade: {params.getRowsAmount()} X {params.getColumnsAmount()}
+          <Field />
+          <Field  opened />
+          <Field opened nearMines={1}/>
+          <Field opened nearMines={2}/>
+          <Field opened nearMines={3}/>
+          <Field opened nearMines={6}/>
+          <Field  mined />
+          <Field  opened mined />
+          <Field  opened mined exploded />
           </Text>
       </View>
     )
@@ -17,7 +27,10 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  instructions: {
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
 })
